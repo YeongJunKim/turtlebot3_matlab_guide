325c58e61adb33ff8ad8913cb61d9aab023b5ff6
colson <dud37222000@naver.com>
2020-04-08 01:47:22 +0900

    add mapping function file

---
+++ mapping.m
@@ -0,0 +1,20 @@
+function mapping(hfigure)
+
+
+handles = guidata(hfigure);
+
+
+
+
+% robot_position_mapping
+% 
+% robot's state (x, y, theta)
+% handles.app.data.state = (:, robot_index)
+% The robot_index.              : handles.app.data.robot_num (1:robot_num)
+
+
+% 
+
+
+guidata(hfigure, handles);
+end
